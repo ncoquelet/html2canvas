e5b64f8ae5bd84ed6c40096b2e19100698cdd9db
nicolas coquelet <nicolas.coquelet@tryane.com>
2015-08-27 17:50:53 +0200

    ignore svg element during clone

--- src/nodeparser.js
+++ src/nodeparser.js
@@ -816,8 +816,12 @@ function getWidth(border) {
     return border.width;
 }
 
+function nonIgnoredSVGElement(nodeContainer) {
+    return !nodeContainer.node.ownerSVGElement;
+}
+
 function nonIgnoredElement(nodeContainer) {
-    return (nodeContainer.node.nodeType !== Node.ELEMENT_NODE || ["SCRIPT", "HEAD", "TITLE", "OBJECT", "BR", "OPTION"].indexOf(nodeContainer.node.nodeName) === -1);
+    return (nodeContainer.node.nodeType !== Node.ELEMENT_NODE || (["SCRIPT", "HEAD", "TITLE", "OBJECT", "BR", "OPTION"].indexOf(nodeContainer.node.nodeName) === -1 && nonIgnoredSVGElement(nodeContainer)));
 }
 
 function flatten(arrays) {
